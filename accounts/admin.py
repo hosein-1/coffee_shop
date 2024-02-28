3f5120ca15fed18ea9b1e1acafe10efdfec1b159
hosein <hoseinlaki16@gmail.com>
2024-02-28 18:27:27 +0330

    add customuser model to admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email_confirmed']
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('email_confirmed',)}),
+    )
