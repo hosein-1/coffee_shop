f1e3dd2e99c0bd66e17e52c56525426432cbf15d
hosein <hoseinlaki16@gmail.com>
2024-02-28 18:16:15 +0330

    config email settings

--- config/settings.py
+++ config/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -130,3 +130,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CustomUser settings
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# Email Config settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'hosein.noob.dj@gmail.com'
+EMAIL_HOST_PASSWORD = 'cirx fxuu sung uctz'
