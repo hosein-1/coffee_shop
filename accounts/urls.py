2cbc97ff253ac7bab81d7872690921d2b4cd0eaf
hosein <hoseinlaki16@gmail.com>
2024-03-01 18:45:00 +0330

    implement password_reset system

--- accounts/urls.py
+++ accounts/urls.py
@@ -17,4 +17,10 @@ urlpatterns = [
     path('password-change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
          name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
