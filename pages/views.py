6f6a0f8e1c71a432bde37f0aabb61cf261635835
hosein <hoseinlaki16@gmail.com>
2024-03-02 21:40:36 +0330

    add about view and it's template

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generics import TemplateView
 
-# Create your views here.
+
+class AboutUsView(TemplateView):
+    template_name = 'pages/about.html'
+
