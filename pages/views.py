d0b6d9e4e31787f0a4cae9b866b30253cf285b5d
hosein <hoseinlaki16@gmail.com>
2024-03-02 21:41:15 +0330

    add contact_us view and it's template

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,6 @@ from django.views.generics import TemplateView
 class AboutUsView(TemplateView):
     template_name = 'pages/about.html'
 
+
+class ContactUs(TemplateView):
+    template_name = 'pages/contact_us.html'
