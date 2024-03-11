d7699eef897ec7c18dca44fef2534ca967e917e7
hosein <hoseinlaki16@gmail.com>
2024-03-11 23:04:55 +0330

    add product model and fields

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from ckeditor.fields import RichTextField
+from django_jalali.db import models as jmodels
+from django.utils import timezone
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=500)
+    description = RichTextField()
+    price = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='product/product_images')
+    active = models.BooleanField(default=True)
+    datetime_created = jmodels.jDateTimeField(default=timezone.now)
+    datetime_modified = jmodels.jDateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
