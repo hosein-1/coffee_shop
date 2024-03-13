7eaae835dc3e61cb8d86fb48b24c10b1cfde8d89
hosein <hoseinlaki16@gmail.com>
2024-03-13 18:34:37 +0330

    change the url path for accounts app and fix some errors in the registration templates

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('products.urls')),
     path('', include('pages.urls')),
 ]
