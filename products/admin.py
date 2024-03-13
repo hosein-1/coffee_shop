32e437290f73c1a2e8d06746cb13cf6d2ecd1d4c
hosein <hoseinlaki16@gmail.com>
2024-03-13 20:59:20 +0330

    add comment model to the admin panel

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django_jalali.admin.filters import JDateFieldListFilter
 import django_jalali.admin as jadmin
 
-from .models import Product
+from .models import Product, Comment
 
 
 @admin.register(Product)
@@ -10,3 +10,8 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'datetime_created']
     list_editable = ['active', ]
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'active']
+    list_editable = ['active']
