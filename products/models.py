54111e082d3c205c0f1c49e2637da2f4b308dfea
hosein <hoseinlaki16@gmail.com>
2024-03-13 18:52:26 +0330

    add the get_absolute_url for product model

--- products/models.py
+++ products/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+from django.shortcuts import reverse
 from django_jalali.db import models as jmodels
+
+from ckeditor.fields import RichTextField
 
 
 class Product(models.Model):
@@ -14,3 +16,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('products:product_detail', args=[self.pk])
