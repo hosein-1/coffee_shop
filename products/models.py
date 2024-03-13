9948d1886b969a4006ebc95c5058c6c7a8725b17
hosein <hoseinlaki16@gmail.com>
2024-03-13 19:22:18 +0330

    create comment model for product

--- products/models.py
+++ products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.shortcuts import reverse
 from django_jalali.db import models as jmodels
+from django.contrib.auth import get_user_model
 
 from ckeditor.fields import RichTextField
 
@@ -19,3 +20,16 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         return reverse('products:product_detail', args=[self.pk])
+
+
+class Comment(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='comments')
+    body = models.TextField()
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.first_name + ':' + self.product.title
+
