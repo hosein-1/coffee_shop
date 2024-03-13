a8f0e9b56858d6120be0ed4e43eeab3858664be9
hosein <hoseinlaki16@gmail.com>
2024-03-13 18:32:00 +0330

    add the product model to the admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django_jalali.admin.filters import JDateFieldListFilter
+import django_jalali.admin as jadmin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active', 'datetime_created']
+    list_editable = ['active', ]
+
