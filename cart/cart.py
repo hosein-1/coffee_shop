8ea852c0440c0d51d73a0cfd52cafbb15be10463
hosein <hoseinlaki16@gmail.com>
2024-03-15 13:22:52 +0330

    add remove method to remove a product in cart

--- cart/cart.py
+++ cart/cart.py
@@ -19,5 +19,12 @@ class Cart:
 
         self.save()
 
+    def remove(self, product):
+        product_id = str(product)
+        if self.cart[product_id]['quantity'] > 0:
+            self.cart[product_id]['quantity'] -= 1
+
+        self.save()
+
     def save(self):
         self.session.modified = True
