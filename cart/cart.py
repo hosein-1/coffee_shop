4edf7f5faa9de92f3d64574aa90a61807b786206
hosein <hoseinlaki16@gmail.com>
2024-03-15 13:19:29 +0330

    create method to add product to the cart

--- cart/cart.py
+++ cart/cart.py
@@ -10,5 +10,14 @@ class Cart:
             cart = self.session['cart'] = {}
             self.cart = cart
 
+    def add(self, product):
+        product_id = str(product)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 1}
+        else:
+            self.cart[product_id]['quantity'] += 1
+
+        self.save()
+
     def save(self):
         self.session.modified = True
