aa63a1abd9981d57366af5c263c8ffb30abdbcbf
hosein <hoseinlaki16@gmail.com>
2024-03-15 13:29:11 +0330

    change the remove method name to reduce

--- cart/cart.py
+++ cart/cart.py
@@ -19,7 +19,7 @@ class Cart:
 
         self.save()
 
-    def remove(self, product):
+    def reduce(self, product):
         product_id = str(product)
         if self.cart[product_id]['quantity'] > 0:
             self.cart[product_id]['quantity'] -= 1
