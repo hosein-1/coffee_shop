773c138ec52c42417e5add04c40a345b961fe2c4
hosein <hoseinlaki16@gmail.com>
2024-04-08 12:58:42 +0330

    add tailwind code for password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(success_url=reverse_lazy('accounts:password_reset_done')), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('password-reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(),
+         auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('accounts:password_reset_complete')),
          name='password_reset_confirm'),
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('account-info/', views.AccountInfoView.as_view(), name='account_info'),
