e2e4d67e39fbbc24670c07078feb962f4b4fed36
hosein <hoseinlaki16@gmail.com>
2024-04-16 14:35:43 +0330

    implement purchase order

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
+from . import views
 
 urlpatterns = [
-    
+    path('order-create/', views.order_create_view, name='order_create'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+from cart.cart import Cart
+from .forms import OrderForm
+from .models import OrderItem
+
+
+@login_required
+def order_create_view(request):
+    order_form = OrderForm()
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, 'سبد خرید شما خالی است، لطفا محصولی را به سبد خرید اضافه کنید.')
+        return redirect('products:products_list')
+
+    if request.method == 'POST':
+        print(request.POST)
+        order_form = OrderForm(request.POST)
+
+        if order_form.is_valid():
+            order_object = order_form.save(commit=False)
+            order_object.user = request.user
+            order_object.save()
+
+            for item in cart:
+                product = item['product_obj']
+                OrderItem.objects.create(
+                    order=order_object,
+                    product=product,
+                    quantity=item['quantity'],
+                    price=product.price,
+                )
+
+            cart.clear()
+
+            request.session['order_id'] = order_object.id
+            return redirect('payment:payment_process')
+
+    return render(request, 'orders/order_create.html', {'form': order_form})
