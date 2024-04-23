7747f7034cd74b7450e8d6e6a5bec01ad132a528
hosein <hoseinlaki16@gmail.com>
2024-04-23 23:40:14 +0330

    add order and orderitem model to the admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price', ]
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'is_paid', ]
+
+    inlines = [
+        OrderItemInline
+    ]
